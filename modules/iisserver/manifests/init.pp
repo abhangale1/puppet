ff4a68e806844a0509af9e4fd95b08a08da62e81
abhangale1 <avinashbhangale1980@gmail.com>
2017-05-27 20:26:47 +0000

    Initial commit of Puppet files

---
+++ modules/iisserver/manifests/init.pp
@@ -0,0 +1,18 @@
+class iisserver {
+   $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net45','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-45-ASPNET','WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
+
+   windowsfeature { $iis_features:
+      ensure => present,
+}
+  }
+
+  windowsfeature { 'Web-WebServer':
+    ensure => present,
+    installsubfeatures => true,
+    installmanagementtools => true
+  }
+
+  file {
+    'c:/inetpub/wwwroot/info.aspx': content => '<%@ Page Trace="true"  Language="C#" ContentType="text/html" ResponseEncoding="utf-8" %>',
+        require => windowsfeature['Web-WebServer'],
+  }
